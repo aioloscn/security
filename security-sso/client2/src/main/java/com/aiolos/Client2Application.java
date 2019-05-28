1568a6bb30dcff7ae8d1e85c7c467282853caad6
aioloscn <756243011@qq.com>
2019-05-28 10:19:03 +0800

    'ssoCompleted'

--- security-sso/client2/src/main/java/com/aiolos/Client2Application.java
+++ security-sso/client2/src/main/java/com/aiolos/Client2Application.java
@@ -1,20 +1,18 @@
-package com.aiolos.sso.client;
+package com.aiolos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Aiolos
- * @date 2019-05-25 9:13
+ * @date 2019-05-28 10:00
  */
 @SpringBootApplication
 @RestController
-@EnableOAuth2Sso
-public class SsoClient1Application {
+public class Client2Application {
 
     @GetMapping("/user")
     public Authentication user(Authentication user) {
@@ -22,6 +20,6 @@ public class SsoClient1Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(SsoClient1Application.class, args);
+        SpringApplication.run(Client2Application.class, args);
     }
 }
--- security-sso/server/src/main/java/com/aiolos/config/AuthorizationServerConfig.java
+++ security-sso/server/src/main/java/com/aiolos/config/AuthorizationServerConfig.java
@@ -1,70 +1,58 @@
-package com.aiolos.sso.server.config;
+package com.aiolos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-/**
- * 认证服务器
- * @author Aiolos
- * @date 2019-05-24 16:50
- */
+import java.util.concurrent.TimeUnit;
+
+
 @Configuration
 @EnableAuthorizationServer
-public class SsoAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    /**
-     * 配置给哪些应用发令牌
-     * @param clients
-     * @throws Exception
-     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
+        // 定义了两个客户端应用的通行证
         clients.inMemory()
                 .withClient("client1")
-                .secret("clientSecret1")
-                .authorizedGrantTypes("refresh_token", "password", "authorization_code")
+                .secret(new BCryptPasswordEncoder().encode("clientSecret1"))
+                .authorizedGrantTypes("authorization_code", "refresh_token")
                 .scopes("all")
+                .autoApprove(true)
                 .and()
                 .withClient("client2")
-                .secret("clientSecret2")
-                .authorizedGrantTypes("refresh_token", "password", "authorization_code")
-                .scopes("all");
+                .secret(new BCryptPasswordEncoder().encode("clientSecret2"))
+                .authorizedGrantTypes("authorization_code", "refresh_token")
+                .scopes("all")
+                .autoApprove(true);
     }
 
-    /**
-     * 使用jwt令牌
-     * @param endpoints
-     * @throws Exception
-     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
         endpoints.tokenStore(jwtTokenStore()).accessTokenConverter(jwtAccessTokenConverter());
+        DefaultTokenServices tokenServices = (DefaultTokenServices) endpoints.getDefaultAuthorizationServerTokenServices();
+        tokenServices.setTokenStore(endpoints.getTokenStore());
+        tokenServices.setSupportRefreshToken(true);
+        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+        tokenServices.setAccessTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(1)); // 一天有效期
+        endpoints.tokenServices(tokenServices);
     }
 
-    /**
-     * 认证服务器的安全配置
-     * @param security
-     * @throws Exception
-     */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-
-        /**
-         * isAuthenticated() 授权表达式
-         * 访问认证服务器的tokenKey的时候需要经过身份认证
-         * tokenKey: aiolos↓
-         * 因为最终认证服务器返回给应用服务器的是jwt，而生成jwt就需要一个密钥去签名
-         * 当应用拿到这个jwt的时候就需要密钥去解析
-         */
         security.tokenKeyAccess("isAuthenticated()");
     }
 
@@ -76,7 +64,8 @@ public class SsoAuthorizationServerConfig extends AuthorizationServerConfigurerA
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter(){
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("aiolos");
+        converter.setSigningKey("testKey");
         return converter;
     }
+
 }
